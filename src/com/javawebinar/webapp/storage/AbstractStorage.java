a7670a9fcd99b4c2969114c3f4b7454d42a71c2c
Sergey Sokolovskii <sergunok1242@gmail.com>
2020-11-01 13:03:03 +0300

    hw5, not broken!

--- src/com/javawebinar/webapp/storage/AbstractStorage.java
+++ src/com/javawebinar/webapp/storage/AbstractStorage.java
@@ -7,39 +7,39 @@ import com.javawebinar.webapp.model.Resume;
 public abstract class AbstractStorage implements Storage {
 
     public void update(Resume resume) {
-        Object searchKey = getSearchKey(resume.getUuid());
-        if (!isContains(searchKey)) {
-            throw new NotExistStorageException(resume.getUuid());
-        } else {
-            updateDiff(resume, searchKey);
-        }
+        Object searchKey = getExistKey(resume.getUuid());
+        updateDiff(resume, searchKey);
     }
 
     public void save(Resume resume) {
-        Object searchKey = getSearchKey(resume.getUuid());
-        if (isContains(searchKey)) {
-            throw new ExistStorageException(resume.getUuid());
-        } else {
-            saveDiff(resume, searchKey);
-        }
+        Object searchKey = getNotExistKey(resume.getUuid());
+        saveDiff(resume, searchKey);
     }
 
     public Resume get(String uuid) {
-        Object searchKey = getSearchKey(uuid);
-        if (!isContains(searchKey)) {
-            throw new NotExistStorageException(uuid);
-        } else {
-            return getDiff(searchKey, uuid);
-        }
+        Object searchKey = getExistKey(uuid);
+        return getDiff(searchKey, uuid);
     }
 
     public void delete(String uuid) {
+        Object searchKey = getExistKey(uuid);
+        deleteDiff(searchKey, uuid);
+    }
+
+    private Object getExistKey(String uuid) {
         Object searchKey = getSearchKey(uuid);
         if (!isContains(searchKey)) {
             throw new NotExistStorageException(uuid);
-        } else {
-            deleteDiff(searchKey, uuid);
         }
+        return searchKey;
+    }
+
+    private Object getNotExistKey(String uuid) {
+        Object searchKey = getSearchKey(uuid);
+        if (isContains(searchKey)) {
+            throw new ExistStorageException(uuid);
+        }
+        return searchKey;
     }
 
     protected abstract boolean isContains(Object searchKey);
