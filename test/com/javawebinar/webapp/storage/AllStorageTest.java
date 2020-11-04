25953c934d2c18a008c0615638629649dd7241b0
Sergey Sokolovskii <sergunok1242@gmail.com>
2020-11-04 14:12:19 +0300

    hw6

---
+++ test/com/javawebinar/webapp/storage/AllStorageTest.java
@@ -0,0 +1,15 @@
+package com.javawebinar.webapp.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        ListStorageTest.class,
+        MapUuidStorageTest.class,
+        MapStorageTest.class
+})
+public class AllStorageTest {
+}
